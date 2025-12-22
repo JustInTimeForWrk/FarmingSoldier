e885dd0afa15c004f54ab9d00efa4b361e5dcf6e
riegj8298 <riegj8298@AZRP-STD-e252.wrdsb.ca>
2025-12-22 14:13:55 -0500

    work done

--- src/main/java/scripts/HostileMovement.java
+++ src/main/java/scripts/HostileMovement.java
@@ -7,11 +7,11 @@ import physics.Rigidbody;
 import physics.TileCollider;
 import util.Component;
 import util.Entity;
-import util.Player;
-import util.SceneManager;
 import view.Window;
+import java.util.HashMap;
 
-import java.awt.*;
+import java.awt.image.BufferedImage;
+import view.SpriteRenderer;
 
 public class HostileMovement extends Component {
 
@@ -22,8 +22,18 @@ public class HostileMovement extends Component {
     float speed = 0.1f;
     float speedCap = 2f;
     public Rigidbody rb;
-
-
+    public HashMap<String, BufferedImage> hostileImages = new HashMap();
+    private String[] imageNames = {"hostile_down","hostile_down_left","hostile_left","hostile_up_left",
+                                   "hostile_up","hostile_up_right","hostile_right","hostile_down_right"};
+    private int frameCounter = 0;
+    private int hostileFrame = 1;
+    private SpriteRenderer sr;
+    String imagePath = "resources/assets/hostile/hostile_down01.png";
+    
+    Vector2f dir = new Vector2f();
+    String dirStringX = "";
+    String dirStringY = "_down";
+    
     @Override
     public void update() {
         if (playerCollider != null) {
@@ -46,13 +56,46 @@ public class HostileMovement extends Component {
                     }
                 }
 
-                Vector2f dir = (playerCollider.getCenter()).sub(hostileCollider.getCenter());
+                dir = (playerCollider.getCenter()).sub(hostileCollider.getCenter());
                 rb.addToVelocity(dir.mul(speed));
                 if (rb.velocity.length() > (speedCap)) {
                     rb.velocity.normalize().mul(speedCap);
                 }
+                dir.normalize();
+                if (dir.x > 0.5) {
+                    dirStringX = "_right";
+                } else if (dir.x < -0.5) {
+                    dirStringX = "_left";
+                } else {
+                    dirStringX = "";
+                }
+                if (dir.y > 0.5) {
+                    dirStringY = "_down";
+                } else if (dir.y < -0.5){
+                    dirStringY = "_up";
+                } else {
+                    dirStringY = "";
+                }
+                
+                frameCounter++;
+                if (frameCounter >= 16) {
+                    frameCounter -= 16; //changes hostile animation every 16 frames
+
+                    hostileFrame++;
+                    if (hostileFrame >= 5) {
+                            hostileFrame -= 4;
+                    }
+                }
+                
+                imagePath = "resources/assets/hostile/hostile"+dirStringY+dirStringX+"0"+hostileFrame+".png";
+            } else {
+                if (hostileFrame != 1) {
+                    hostileFrame = 1;
+                }
+                imagePath = "resources/assets/hostile/hostile"+dirStringY+dirStringX+"0"+hostileFrame+".png";
             }
-        }
+            
+        } sr.changeImage(hostileImages.get(imagePath));
     }
 
     @Override
@@ -62,6 +105,14 @@ public class HostileMovement extends Component {
         Entity player = entity.parentScene.findEntityByTag("Player");
         playerCollider = player.getComponent(BoxCollider.class);
 
+        for (String str : imageNames) {
+            for (int i = 1; i < 5; i++) {
+                String imagePath = "resources/assets/hostile/"+str+"0"+i+".png";
+                hostileImages.put(imagePath, SpriteRenderer.loadImage(imagePath));
+            }
+        }
+        this.sr = entity.getComponent(SpriteRenderer.class);
+        
         super.init();
     }
 }
